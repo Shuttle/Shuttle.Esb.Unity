6c7f65bd9bd73c9a798f10016b875eff718f8a21
Eben <me@ebenroux.co.za>
2015-12-19 17:26:42 +0200

    - v1.5.1

--- Shuttle.ESB.Unity/Properties/AssemblyInfo.cs
+++ Shuttle.ESB.Unity/Properties/AssemblyInfo.cs
@@ -25,10 +25,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Shuttle.ESB.Unity for .NET Framework 4.6.1")]
 #endif
 
-[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyVersion("1.5.1.0")]
 [assembly: AssemblyCopyright("Copyright © Eben Roux 2010-2015")]
 [assembly: AssemblyProduct("Shuttle.ESB")]
 [assembly: AssemblyCompany("Shuttle")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyInformationalVersion("1.5.0")]
+[assembly: AssemblyInformationalVersion("1.5.1")]
 [assembly: ComVisible(false)]
