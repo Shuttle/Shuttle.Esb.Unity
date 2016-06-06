df5aef5f08cf7907031127a4c2b73ecd27365cdf
Eben <me@ebenroux.co.za>
2016-06-06 18:22:25 +0200

    - using esb resources for logging

--- Shuttle.Esb.Unity/UnityMessageHandlerFactory.cs
+++ Shuttle.Esb.Unity/UnityMessageHandlerFactory.cs
@@ -6,79 +6,87 @@ using Shuttle.Core.Infrastructure;
 
 namespace Shuttle.Esb.Unity
 {
-	public class UnityMessageHandlerFactory : MessageHandlerFactory, IRequireInitialization
-	{
-		private readonly IUnityContainer _container;
-		private static readonly Type Generic = typeof (IMessageHandler<>);
-		private static readonly InjectionMember[] EmptyInjectionMembers = {};
-		private readonly Dictionary<Type, Type> _messageHandlerTypes = new Dictionary<Type, Type>();
-		private readonly ILog _log;
+    public class UnityMessageHandlerFactory : MessageHandlerFactory, IRequireInitialization
+    {
+        private readonly IUnityContainer _container;
+        private static readonly Type Generic = typeof(IMessageHandler<>);
+        private static readonly InjectionMember[] EmptyInjectionMembers = { };
+        private readonly Dictionary<Type, Type> _messageHandlerTypes = new Dictionary<Type, Type>();
+        private readonly ILog _log;
 
-		public UnityMessageHandlerFactory(IUnityContainer container)
-		{
-			Guard.AgainstNull(container, "container");
+        public UnityMessageHandlerFactory(IUnityContainer container)
+        {
+            Guard.AgainstNull(container, "container");
 
-			_container = container;
+            _container = container;
 
-			_log = Log.For(this);
-		}
+            _log = Log.For(this);
+        }
 
-		public override IMessageHandler CreateHandler(object message)
-		{
-			return (IMessageHandler) _container.Resolve(Generic.MakeGenericType(message.GetType()));
-		}
+        public override IMessageHandler CreateHandler(object message)
+        {
+            return (IMessageHandler)_container.Resolve(Generic.MakeGenericType(message.GetType()));
+        }
 
-		public override IEnumerable<Type> MessageTypesHandled
-		{
-			get { return _messageHandlerTypes.Keys; }
-		}
+        public override IEnumerable<Type> MessageTypesHandled
+        {
+            get { return _messageHandlerTypes.Keys; }
+        }
 
-		public void Initialize(IServiceBus bus)
-		{
-			Guard.AgainstNull(bus, "bus");
+        public void Initialize(IServiceBus bus)
+        {
+            Guard.AgainstNull(bus, "bus");
 
-		    _container.RegisterInstance(bus);
+            _container.RegisterInstance(bus);
 
-			RefreshHandledTypes();
-		}
+            RefreshHandledTypes();
+        }
 
-		private void RefreshHandledTypes()
-		{
-			foreach (var containerRegistration in _container.Registrations)
-			{
-				if (containerRegistration.RegisteredType.IsGenericType &&
-				    containerRegistration.RegisteredType.GetGenericTypeDefinition() == Generic)
-				{
-					var messageType = containerRegistration.RegisteredType.GetGenericArguments()[0];
+        private void RefreshHandledTypes()
+        {
+            foreach (var containerRegistration in _container.Registrations)
+            {
+                if (containerRegistration.RegisteredType.IsGenericType &&
+                    containerRegistration.RegisteredType.GetGenericTypeDefinition() == Generic)
+                {
+                    var messageType = containerRegistration.RegisteredType.GetGenericArguments()[0];
 
-					if (_messageHandlerTypes.ContainsKey(messageType))
-					{
-						return;
-					}
+                    if (_messageHandlerTypes.ContainsKey(messageType))
+                    {
+                        return;
+                    }
 
-					_messageHandlerTypes.Add(messageType, containerRegistration.MappedToType);
+                    _messageHandlerTypes.Add(messageType, containerRegistration.MappedToType);
 
-					_log.Information(string.Format(EsbResources.MessageHandlerFactoryHandlerRegistered, messageType.FullName,
-						containerRegistration.MappedToType.FullName));
-				}
-			}
-		}
+                    _log.Information(string.Format(EsbResources.MessageHandlerFactoryHandlerRegistered, messageType.FullName,
+                        containerRegistration.MappedToType.FullName));
+                }
+            }
+        }
 
-		public override IMessageHandlerFactory RegisterHandlers(Assembly assembly)
-		{
-			foreach (var type in new ReflectionService().GetTypes(typeof (IMessageHandler<>), assembly))
-			{
-				var handlerInterfaces = type.InterfacesAssignableTo(Generic);
+        public override IMessageHandlerFactory RegisterHandlers(Assembly assembly)
+        {
+            try
+            {
+                foreach (var type in new ReflectionService().GetTypes(typeof(IMessageHandler<>), assembly))
+                {
+                    var handlerInterfaces = type.InterfacesAssignableTo(Generic);
 
-				foreach (var handlerInterface in handlerInterfaces)
-				{
-					_container.RegisterType(handlerInterface, type, new ContainerControlledLifetimeManager(), EmptyInjectionMembers);
-				}
-			}
+                    foreach (var handlerInterface in handlerInterfaces)
+                    {
+                        _container.RegisterType(handlerInterface, type, new ContainerControlledLifetimeManager(), EmptyInjectionMembers);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _log.Warning(string.Format(EsbResources.RegisterHandlersException, assembly.FullName,
+                    ex.AllMessages()));
+            }
 
-			RefreshHandledTypes();
+            RefreshHandledTypes();
 
-			return this;
-		}
-	}
+            return this;
+        }
+    }
 }
